33561198620f2721959055ab8dbb12abe2a5d03d
Elan Ruusamäe <glen@pld-linux.org>
2009-10-14 12:38:09 +0000

    - cosmetics
    Changed files:
        stanse.sh -> 1.6

--- stanse.sh
+++ stanse.sh
@@ -1,21 +1,16 @@
 #!/bin/sh -e
-
 # Set paths
-
 CLASSPATH=$(build-classpath-directory /usr/share/stanse/lib)
 CLASSPATH=$CLASSPATH:$(find-jar stanse)
 export CLASSPATH
 
-PATH=$PATH:/usr/share/stanse/data/bin
-export PATH
+export PATH=$PATH:/usr/share/stanse/data/bin
 
 # Initialize STANSE_HOME
-
-STANSE_HOME=${STANSE_HOME:-"$HOME/.stanse"}
-export STANSE_HOME
+export STANSE_HOME=${STANSE_HOME:-"$HOME/.stanse"}
 mkdir -p $STANSE_HOME
 [ -e "$STANSE_HOME/bin" ] || ln -s /usr/share/stanse/bin $STANSE_HOME/bin
 [ -e "$STANSE_HOME/data" ] || cp -a /usr/share/stanse/data $STANSE_HOME/data
 [ -e "$STANSE_HOME/properties.xml" ] || cp -a /usr/share/stanse/properties.xml $STANSE_HOME/properties.xml
 
-java cz.muni.stanse.Stanse $@
+exec java cz.muni.stanse.Stanse "$@"

2ea7260b38161a8dfe7918f6bb91c2bb1256ffa5
pawelz <pawelz@pld-linux.org>
2009-10-14 08:15:40 +0000

    - new location for bundled jars (fixed CLASSPATH) - new location for perl scripts (export PATH) - initialize STANSE_HOME (link bin, copy data and properties from _datadir)
    Changed files:
        stanse.sh -> 1.5

--- stanse.sh
+++ stanse.sh
@@ -1,9 +1,21 @@
-#!/bin/sh
+#!/bin/sh -e
 
-CLASSPATH=$(build-classpath-directory /usr/share/java/stanse):$(find-jar stanse)
+# Set paths
+
+CLASSPATH=$(build-classpath-directory /usr/share/stanse/lib)
+CLASSPATH=$CLASSPATH:$(find-jar stanse)
 export CLASSPATH
+
+PATH=$PATH:/usr/share/stanse/data/bin
+export PATH
+
+# Initialize STANSE_HOME
+
 STANSE_HOME=${STANSE_HOME:-"$HOME/.stanse"}
 export STANSE_HOME
 mkdir -p $STANSE_HOME
+[ -e "$STANSE_HOME/bin" ] || ln -s /usr/share/stanse/bin $STANSE_HOME/bin
+[ -e "$STANSE_HOME/data" ] || cp -a /usr/share/stanse/data $STANSE_HOME/data
+[ -e "$STANSE_HOME/properties.xml" ] || cp -a /usr/share/stanse/properties.xml $STANSE_HOME/properties.xml
 
 java cz.muni.stanse.Stanse $@

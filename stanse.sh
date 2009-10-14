7279ccc4f1402860ddd0debad2ddb8bf89c439aa
pawelz <pawelz@pld-linux.org>
2009-10-14 07:11:51 +0000

    - mkdir -p
    Changed files:
        stanse.sh -> 1.4

--- stanse.sh
+++ stanse.sh
@@ -4,6 +4,6 @@ CLASSPATH=$(build-classpath-directory /usr/share/java/stanse):$(find-jar stanse)
 export CLASSPATH
 STANSE_HOME=${STANSE_HOME:-"$HOME/.stanse"}
 export STANSE_HOME
-mkdir $STANSE_HOME
+mkdir -p $STANSE_HOME
 
 java cz.muni.stanse.Stanse $@
